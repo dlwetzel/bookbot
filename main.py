379e45631d7c1e0d730e0a6679bbd5d46966993d
dlwetzel <dlwetzel23@yahoo.com>
2024-06-24 14:06:19 -0600

    created read_file and count_words functions

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hello world")
+def main():
+    
+    frankenstein_path = "books/frankenstein.txt"
+
+    
+    def read_file():
+        with open(frankenstein_path) as f:
+            file_contents = f.read()
+        return file_contents
+    
+    def count_words():
+        file_contents_str = read_file()
+        words = file_contents_str.split()
+        print(len(words))
+    
+    count_words()
+    
+main()
